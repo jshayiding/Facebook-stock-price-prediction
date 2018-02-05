211b53a88c75c11a125cd23bc936c20a7d1c26c6
jshahidin <jian_liangli@126.com>
2018-02-05 16:27:36 +0100

    update on description of the project

--- facebook_stockprice_predictor/FB_stockprice_predict.py
+++ facebook_stockprice_predictor/FB_stockprice_predict.py
@@ -1,14 +1,12 @@
 """
 Description:
 
-This project dedicated to implement web based application in python development
-environment, and whole environement has been set up with Flask.
-In this project I programmatically collect last 365 days of facebook stock price data
-using python modules bs4::BeautifulSoup and request and write to temporary csv file.
-based on the data collected, opening price for requested time period is predicted 
-by using linear regression model by using sklearn::linear_model python module. 
-Whenever the stock prediction for facebook stock completed, temporary historical data
-that collected from web API will be deleted.
+This project dedicated to implement web based application in python environment, and whole environement has been set up with Flask.
+In this project I programmatically collect last 365 days of facebook stock price data using python modules bs4::BeautifulSoup and
+request and write to temporary csv file by using csv, Request module. Based on the data collected, opening price for requested time
+period is predicted by using Logistic regression model by using sklearn::linear_model python module. confidence accuracy of logistic
+regression and SGD classifier are implemented. For displaying the stock chart on respective web page, I used dygraph java scripts library.
+Whenever the stock prediction for facebook stock completed, temporary historical data that collected from web API will be deleted.
 
 
 Python version used 3.6
